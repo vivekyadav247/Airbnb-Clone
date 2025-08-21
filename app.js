d0ea00927ecc1cf16378a05a598af63bedc306de
Vivek Yadav <vivekyad240706@gmail.com>
2025-08-21 14:57:46 +0530

    Error Resolved

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ const sessionOptions = {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // 1 week
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     httpOnly: true, // security purpose
+    secure: true,
+    samesite: "lax",
   },
 };
 
